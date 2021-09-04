434d8faa98cd7c5f806dc8feea9c244372e44580
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-04 13:34:30 +0700

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './*.html',
+    './src/**/*.jsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
